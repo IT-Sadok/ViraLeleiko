924125f5079c904f59583fa2eb6935f1a9d797f0
Bepa88 <leleiko.vira@gmail.com>
2025-09-26 13:03:22 +0200

    Implement booking system

--- BookingSystem/BookingSystem/BookingSystem/Program.cs
+++ BookingSystem/BookingSystem/BookingSystem/Program.cs
@@ -1,3 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BookingSystem.Models;
 
-Console.WriteLine("Hello, World!");
+var hosts = new List<Host>
+{
+    new Host
+    {
+        Id = 1,
+        Name = "Host Nuture",
+        Apartments = new List<Apartment>
+        {
+            new Apartment { Id = 1, Name = "The Oasis" },
+            new Apartment { Id = 2, Name = "The Park" },
+            new Apartment { Id = 3, Name = "The Forest" }
+        }
+    },
+    new Host
+    {
+        Id = 2,
+        Name = "Host Urban",
+        Apartments = new List<Apartment>
+        {
+            new Apartment { Id = 1, Name = "The Loft Life" },
+            new Apartment { Id = 2, Name = "The Urban Utopia" }
+        }
+    }
+};
+
+Console.WriteLine("Список хостів:");
+foreach (var host in hosts)
+{
+    Console.WriteLine($"{host.Id}. {host.Name}");
+}
+
+Console.Write("\nВведіть ID хоста: ");
+if (int.TryParse(Console.ReadLine(), out int hostId))
+{
+    var selectedHost = hosts.FirstOrDefault(h => h.Id == hostId);
+
+    if (selectedHost != null)
+    {
+        Console.WriteLine($"\nАпартаменти {selectedHost.Name}:");
+        foreach (var apt in selectedHost.Apartments)
+        {
+            Console.WriteLine($"- {apt.Id}: {apt.Name}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Хоста з таким ID не знайдено.");
+    }
+}

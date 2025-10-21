5f0b12e4ecbc26bc917dddfc5dab896ee90fba6b
Bepa88 <leleiko.vira@gmail.com>
2025-10-21 12:49:07 +0200

    Add CRUD operations for Host

--- BookingSystem/BookingSystem/BookingSystem/Program.cs
+++ BookingSystem/BookingSystem/BookingSystem/Program.cs
@@ -1,51 +1,111 @@
 ﻿using BookingSystem.Models;
+using BookingSystem.Repositories;
+using BookingSystem.Services;
 
-var hosts = new List<Host>
+var repository = new HostRepository();
+var hostService = new HostService(repository);
+
+bool isRunning = true;
+
+while (isRunning)
 {
-    new Host
+    Console.WriteLine();
+    Console.WriteLine("Вибери потрібний пункт у меню та натисни відповідну цифру:");
+    Console.WriteLine(string.Join(Environment.NewLine, new[]
     {
-        Id = 1,
-        Name = "Host Nuture",
-        Apartments = new List<Apartment>
-        {
-            new Apartment { Id = 1, Name = "The Oasis" },
-            new Apartment { Id = 2, Name = "The Park" },
-            new Apartment { Id = 3, Name = "The Forest" }
-        }
-    },
-    new Host
+        "1. Список хостів",
+        "2. Показати хоста за ID",
+        "3. Додавання нового хоста",
+        "4. Редагування хоста",
+        "5. Видалення хоста",
+        "0. Вихід"
+    }));
+
+    Console.Write("\nВаш вибір: ");
+    if (int.TryParse(Console.ReadLine(), out int menuId))
     {
-        Id = 2,
-        Name = "Host Urban",
-        Apartments = new List<Apartment>
+        switch (menuId)
         {
-            new Apartment { Id = 1, Name = "The Loft Life" },
-            new Apartment { Id = 2, Name = "The Urban Utopia" }
-        }
-    }
-};
+            case 1:
+                Console.WriteLine("\nСписок хостів:");
+                var hosts = hostService.GetAllHosts();
+                foreach (var h in hosts)
+                {
+                    Console.WriteLine($"{h.Id}. {h.Name}");
+                }
+                break;
 
-Console.WriteLine("Список хостів:");
-foreach (var host in hosts)
-{
-    Console.WriteLine($"{host.Id}. {host.Name}");
-}
+            case 2:
+                Console.Write("\nВведіть ID хоста: ");
+                if (int.TryParse(Console.ReadLine(), out int hostId))
+                {
+                    var host = hostService.GetHostById(hostId);
+                    if (host != null)
+                    {
+                        Console.WriteLine($"ID: {host.Id}, Ім'я: {host.Name}");
+                        Console.WriteLine("Апартаменти:");
+                        foreach (var apt in host.Apartments)
+                            Console.WriteLine($"  - {apt.Id}: {apt.Name}");
+                    }
+                    else
+                        Console.WriteLine("Хоста з таким ID не знайдено.");
+                }
+                break;
 
-Console.Write("\nВведіть ID хоста: ");
-if (int.TryParse(Console.ReadLine(), out int hostId))
-{
-    var selectedHost = hosts.FirstOrDefault(h => h.Id == hostId);
+            case 3:
+                Console.Write("\nВведіть ім'я нового хоста: ");
+                string? name = Console.ReadLine();
+                var newHost = new Host { Name = name ?? "Без назви" };
+                hostService.AddHost(newHost);
+                Console.WriteLine("Хоста додано!");
+                break;
 
-    if (selectedHost != null)
-    {
-        Console.WriteLine($"\nАпартаменти {selectedHost.Name}:");
-        foreach (var apt in selectedHost.Apartments)
-        {
-            Console.WriteLine($"- {apt.Id}: {apt.Name}");
+            case 4:
+                Console.Write("\nВведіть ID хоста для редагування: ");
+                if (int.TryParse(Console.ReadLine(), out int editId))
+                {
+                    var existingHost = hostService.GetHostById(editId);
+                    if (existingHost != null)
+                    {
+                        Console.Write($"Введіть нове ім'я (поточне: {existingHost.Name}): ");
+                        string? newName = Console.ReadLine();
+                        existingHost.Name = string.IsNullOrWhiteSpace(newName) ? existingHost.Name : newName;
+
+                        if (hostService.UpdateHost(existingHost))
+                            Console.WriteLine("Хоста оновлено!");
+                        else
+                            Console.WriteLine("Не вдалося оновити хоста.");
+                    }
+                    else
+                        Console.WriteLine("Хоста не знайдено.");
+                }
+                break;
+
+            case 5:
+                Console.Write("\nВведіть ID хоста для видалення: ");
+                if (int.TryParse(Console.ReadLine(), out int deleteId))
+                {
+                    if (hostService.DeleteHost(deleteId))
+                        Console.WriteLine("Хоста видалено.");
+                    else
+                        Console.WriteLine("Хоста не знайдено.");
+                }
+                break;
+
+            case 0:
+                Console.WriteLine("До побачення!");
+                isRunning = false;
+                break;
+
+            default:
+                Console.WriteLine("Невідомий пункт меню. Спробуйте ще раз.");
+                break;
         }
     }
     else
     {
-        Console.WriteLine("Хоста з таким ID не знайдено.");
+        Console.WriteLine("Невірне введення. Введіть число від 0 до 5.");
     }
 }
+
+
---
+++ BookingSystem/BookingSystem/BookingSystem/Services/HostService.cs
@@ -0,0 +1,23 @@
+using BookingSystem.Models;
+using BookingSystem.Repositories;
+
+namespace BookingSystem.Services;
+
+public class HostService
+{
+    private readonly IHostRepository _repository;
+
+    public HostService(IHostRepository repository)
+    {
+        _repository = repository; 
+    }
+    public IEnumerable<Host> GetAllHosts() => _repository.GetAll();
+
+    public Host? GetHostById(int id) => _repository.GetById(id);
+
+    public void AddHost(Host host) => _repository.Add(host);
+
+    public bool UpdateHost(Host host) => _repository.Update(host);
+
+    public bool DeleteHost(int id) => _repository.Delete(id);
+}
